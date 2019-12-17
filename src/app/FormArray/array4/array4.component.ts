3c683f6f725c9d19b2a89589cb0fe567d84fa9dd
Diego de Fabregas <kangcor@gmail.com>
2019-12-17 23:23:54 +0100

    Let's do this

--- src/app/FormArray/array4/array4.component.ts
+++ src/app/FormArray/array4/array4.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-array4',
-  templateUrl: './array4.component.html',
-  styleUrls: ['./array4.component.css']
+  selector: "app-array4",
+  templateUrl: "./array4.component.html",
+  styleUrls: ["./array4.component.css"]
 })
-export class Array4Component implements OnInit {
+export class Array4Component {
 
-  constructor() { }
+  objectiveForm = {
+    customer: {
+      name: 'Mario',
+      surname: 'Rossi',
+      age: 32,
+      address: [
+        {
+          street: 'Sicilia',
+          number: '91-97',
+          city: 'Barcelona',
+        },
+        {
+          street: 'Urquinaona',
+          number: '6',
+          city: 'Barcelona',
+        }
+      ],
+    },
+    orders: [
+      {
+        number: 1,
+        name: 'Pizza',
+        ingredients: [
+          'Tomate',
+          'Gorgonzola',
+          'Bacon',
+          'Pulled pork'
+        ]
+      },
+      {
+        number: 2,
+        name: 'Nuggets',
+        ingredients: [
+          'Nuggets',
+          'BBQ',
+        ]
+      }
+    ]
+  };
 
-  ngOnInit() {
-  }
+  constructor(private fb: FormBuilder) {}
+
+  myForm = this.fb.group({});
 
 }
